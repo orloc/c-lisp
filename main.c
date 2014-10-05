9506ec406923e3a511647500a8b2d33b0e05623c
Grant Tepper <grant.tepper@gmail.com>
2014-10-05 06:59:17 -0400

    more updates to head and tail functions

--- main.c
+++ main.c
@@ -267,3 +267,30 @@ lval* builtin_op(lval* a, char* op){
     return x;
 }
 
+lval* builtin_head(lval* a) {
+    if (a->count != 1){
+        lval_del(a);
+        return lval_err("Function 'head' passed too many args!");
+    }
+
+    if (a->cell[0]->type != LVAL_QEXPR){
+        lval_del(a);
+        return lval_err("Function 'head' passed incorrect typse!");
+    }
+
+    if (a->cell[0]->count == 0){
+        lval_del(a);
+        return lval_err("Function 'head' passed {}!");
+    }
+
+    lval* v = lval_take(a, 0);
+
+    while(v->count > 1) {
+        lval_del(lval_pop(v,1));
+    }
+    return v;
+}
+
+lval* builtin_tail(lval* a){
+}
+
--- main.h
+++ main.h
@@ -52,4 +52,6 @@ lval* lval_eval_sexpr(lval* v);
 lval* lval_pop(lval* v, int i);
 lval* lval_take(lval* v, int i);
 lval* builtin_op(lval* a, char* op);
+lval* builtin_head(lval* a);
+lval* builtin_tail(lval* a);
 
