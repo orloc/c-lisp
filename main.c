132524f258577d93ff7253a3d558d4a733369e91
Grant Tepper <grant.tepper@gmail.com>
2014-10-05 05:00:51 -0400

    adding more functions, no more syntax errors or type changes to be made! implemetning lval_read and print and println

--- main.c
+++ main.c
@@ -105,17 +105,29 @@ double max (double x, double y){
     return x;
 }
 
-void lval_print(lval v){
-    switch (v.type) {
-        case LVAL_NUM: printf("%li", v.num);break;
-        case LVAL_ERR:
-            if (v.err == LERR_DIV_ZERO) { printf("Error: Division by Zero!"); }
-            if (v.err == LERR_BAD_OP) { printf("Error: Division by Zero!"); }
-            if (v.err == LERR_BAD_NUM) { printf("Error: Division by Zero!"); }
-            break;
+void lval_expr_print(lval* v, char open, char close){
+    putchar(open);
+
+    for (int i = 0; i < v->count; i++) {
+        lval_print(v->cell[i]);
+
+    // dont print trailing space
+        if (i != (v->count-1)){
+            putchar(' ');
+        }
+    }
+    putchar(close);
+}
+
+void lval_print(lval* v){
+    switch (v->type) {
+        case LVAL_NUM: printf("%li", v->num); break;
+        case LVAL_ERR: printf("Error: %s", v->err); break;
+        case LVAL_SYM: printf("%s", v->sym); break;
+        case LVAL_SEXPR: lval_expr_print(v, '(', ')'); break;
     }
 }
- void lval_println(lval v) {
+ void lval_println(lval* v) {
      lval_print(v);
      putchar('\n');
  }
